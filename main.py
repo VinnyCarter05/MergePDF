95e45b1f1683f7ffb9c0577722a07a300b28c1ab
VinnyCarter05 <vinnycarter05@gmail.com>
2021-01-05 15:37:12 -0500

    added debugging

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from PyQt5 import QtCore as qtc, QtGui as qtg, QtWidgets as qtw
 import sys
+import logging
 
 from windows.mainwindow import MainWindow
 
+
+
 if __name__ == '__main__':
+    logging.basicConfig(filename='MergePDF.log', encoding='utf-8', level=logging.DEBUG)
+
     app = qtw.QApplication(sys.argv)
     window = MainWindow()
        
--- windows/mainwindow.py
+++ windows/mainwindow.py
@@ -1,13 +1,36 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 from windows.mainwindow_ui import Ui_MainWindow
+import logging
 
 class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
+        # variables
+        self.PDF_list = []
+        self.PDF_count = 0 # number of PDFs
+
         self.setupUi(self)
         self.show()
 
         # setting drag drop mode
         self.listWidget_PDF.setDragDropMode(QtWidgets.QAbstractItemView.InternalMove)
+        self.listWidget_PDF.setSortingEnabled(True)
+
+        # getting PDFlist
+        self.get_PDF_list_and_PDF_count()
+
+    def get_PDF_list_and_PDF_count(self):
+        self.PDF_count = self.listWidget_PDF.count()
+        self.PDF_list = []
+        logging.debug(f'PDF_count = {self.PDF_count}')
+        for item in range(self.PDF_count):
+            self.PDF_list.append (self.listWidget_PDF.item(item).text())
+            logging.debug(f'list widget PDF {item} = {self.PDF_list[item]}')
+
+    def keyPressEvent(self, e):
+        if e.key() == QtCore.Qt.Key_Escape:
+            self.close()
+
+
